ffeacfdc31a1e9ee97d6bfb4eec03cd5683f8f8c
DmitryBaranov77 <zyzychka1@gmail.com>
2021-10-07 19:49:28 +0300

    Testing vue.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,23 +3,24 @@ let resource = Vue.resource('/library');
 
 Vue.component('book-row', {
     props: ['book'],
-    template: '<div>{{ book.id }}  {{ book.author }}  {{ book.name }}  {{ book.publicationYear }}  {{ book.pages }}</div>'
+    template: '<div> {{ book.id }} {{ book.author }}  {{ book.name }}  {{ book.publicationYear }}  {{ book.pages }}</div>'
 
 })
 
 Vue.component('books-list', {
     props: ['books'],
-    template: '<div><book-row v-for="book in books" :key="book.id" :book="book"/></div>'
+    template: '<div><book-row v-for="book in books" :key="book.id" :book="book"/></div>',
+    created: function (){
+        resource.get().then(resultList =>
+            resultList.json().then(data =>
+                data.forEach(book => this.books.push(book))))
+    }
 });
 
 let app = new Vue({
     el: '#app',
     template: '<books-list :books="books" />',
     data: {
-        books: [
-            {id: '1', author: 'Стивен Кинг', name: 'Мизери',publicationYear: '1990', pages: '300'},
-            {id: '2', author: 'Пушкин', name: 'Книга 1',publicationYear: '1500', pages: '20'},
-            {id: '3', author: 'Толстой', name: 'Книга 2',publicationYear: '2000', pages: '2102'}
-        ]
+        books: []
     }
 });
